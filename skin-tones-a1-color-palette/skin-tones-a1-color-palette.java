9883d81d114df6ed421b9032126daf364ae74f19
Christopher Anderton <christopher@deluxive.se>
2016-10-17 21:02:09 +0200

    Added Skin Tones A1 Color Palette

---
+++ skin-tones-a1-color-palette/skin-tones-a1-color-palette.java
@@ -0,0 +1,8 @@
+Color.argb(255,234,216,214); // Almond
+Color.argb(255,220,198,183); // Desert sand
+Color.argb(255,216,203,185); // Pastel gray
+Color.argb(255,204,178,163); // Khaki  
+Color.argb(255,185,159,136); // Pale taupe
+Color.argb(255,103,77,54); // Coffee
+Color.argb(255,132,69,54); // Bole
+Color.argb(255,175,96,100); // Copper rose
---
+++ skin-tones-a1-color-palette/skin-tones-a1-color-palette.swift
@@ -0,0 +1,8 @@
+UIColor(red:0.92, green:0.85, blue:0.84, alpha:1) // Almond
+UIColor(red:0.86, green:0.78, blue:0.72, alpha:1) // Desert sand
+UIColor(red:0.85, green:0.8, blue:0.73, alpha:1) // Pastel gray
+UIColor(red:0.8, green:0.7, blue:0.64, alpha:1) // Khaki  
+UIColor(red:0.73, green:0.62, blue:0.53, alpha:1) // Pale taupe
+UIColor(red:0.4, green:0.3, blue:0.21, alpha:1) // Coffee
+UIColor(red:0.52, green:0.27, blue:0.21, alpha:1) // Bole
+UIColor(red:0.69, green:0.38, blue:0.39, alpha:1) // Copper rose
