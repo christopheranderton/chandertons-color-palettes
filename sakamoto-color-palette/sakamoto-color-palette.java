d0367ad8250e4c6ae94573e2c854fb540b8f81b4
Christopher Anderton <christopher@deluxive.se>
2016-10-17 20:00:37 +0200

    Added Sakamoto Color Palette

---
+++ sakamoto-color-palette/sakamoto-color-palette.java
@@ -0,0 +1,15 @@
+Color.argb(255,18,18,18); // Onyx
+Color.argb(255,62,62,62); // Rifle green
+Color.argb(255,140,140,140); // Taupe gray
+Color.argb(255,190,190,190); // Gray 
+Color.argb(255,229,229,229); // Platinum
+Color.argb(255,247,247,247); // White smoke
+Color.argb(255,202,69,62); // Jasper
+Color.argb(255,251,84,76); // Tomato
+Color.argb(255,254,157,152); // Light salmon pink
+Color.argb(255,207,154,36); // Goldenrod
+Color.argb(255,255,190,47); // Saffron
+Color.argb(255,255,216,129); // Jasmine
+Color.argb(255,21,139,68); // North Texas Green
+Color.argb(255,35,192,98); // Medium sea green
+Color.argb(255,113,217,154); // Medium aquamarine
---
+++ sakamoto-color-palette/sakamoto-color-palette.m
@@ -0,0 +1,15 @@
+[UIColor colorWithRed:0.07 green:0.07 blue:0.07 alpha:1]; // Onyx
+[UIColor colorWithRed:0.24 green:0.24 blue:0.24 alpha:1]; // Rifle green
+[UIColor colorWithRed:0.55 green:0.55 blue:0.55 alpha:1]; // Taupe gray
+[UIColor colorWithRed:0.74 green:0.74 blue:0.74 alpha:1]; // Gray 
+[UIColor colorWithRed:0.9 green:0.9 blue:0.9 alpha:1]; // Platinum
+[UIColor colorWithRed:0.97 green:0.97 blue:0.97 alpha:1]; // White smoke
+[UIColor colorWithRed:0.79 green:0.27 blue:0.24 alpha:1]; // Jasper
+[UIColor colorWithRed:0.98 green:0.33 blue:0.3 alpha:1]; // Tomato
+[UIColor colorWithRed:1 green:0.61 blue:0.6 alpha:1]; // Light salmon pink
+[UIColor colorWithRed:0.81 green:0.61 blue:0.14 alpha:1]; // Goldenrod
+[UIColor colorWithRed:1 green:0.75 blue:0.18 alpha:1]; // Saffron
+[UIColor colorWithRed:1 green:0.85 blue:0.5 alpha:1]; // Jasmine
+[UIColor colorWithRed:0.08 green:0.54 blue:0.27 alpha:1]; // North Texas Green
+[UIColor colorWithRed:0.14 green:0.75 blue:0.38 alpha:1]; // Medium sea green
+[UIColor colorWithRed:0.44 green:0.85 blue:0.61 alpha:1]; // Medium aquamarine
